80a9d39165ca5d78043bc85b37d4103b881d3af1
kavyakinthali13 <kinthali.kavya@gmail.com>
2020-05-27 21:01:08 +0530

    changes

--- src/main/java/com/learn/Ecommerce/exceptions/InvalidCredentials.java
+++
@@ -1,5 +0,0 @@
-package com.learn.Ecommerce.exceptions;
-
-public class InvalidCredentials extends RuntimeException {
-
-}
--- src/main/java/com/learn/Ecommerce/exceptions/UserNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.learn.Ecommerce.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-
-}
